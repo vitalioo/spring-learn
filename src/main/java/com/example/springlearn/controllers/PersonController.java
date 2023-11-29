ca8d26ff9c77c45d76001e7035870329f1922dd3
v.avdeev <v.avdeev@g.nsu.ru>
2023-11-29 19:24:56 +0300

    Added newPerson, create and delete methods in PersonController

--- src/main/java/com/example/springlearn/controllers/PersonController.java
+++ src/main/java/com/example/springlearn/controllers/PersonController.java
@@ -1,31 +1,42 @@
 package com.example.springlearn.controllers;
 
+import com.example.springlearn.entity.Person;
 import com.example.springlearn.service.PersonService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/person")
 public class PersonController {
-    final PersonService personService;
+    private final PersonService personService;
 
     public PersonController(PersonService PersonService) {
         this.personService = PersonService;
     }
 
-    @RequestMapping(value = "/show", method = RequestMethod.GET)
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping("/new")
+    public String create(@ModelAttribute Person person) {
+        personService.save(person);
+        return "people/successCreatePage";
+    }
+
+    @GetMapping()
     public String show(Model model) {
-        model.addAttribute("person", personService.show());
+        model.addAttribute("person", personService.findAll());
         return "people/show";
     }
 
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public String index(@RequestParam(defaultValue = "0 ") int id, Model model) {
+    @GetMapping( "/index")
+    public String index(@RequestParam(defaultValue = "0") int id, Model model) {
         try {
-            model.addAttribute("person", personService.index(id));
+            model.addAttribute("person", personService.findOne(id));
             return "people/show";
         } catch (Exception exception) {
             model.addAttribute("exception", "Произошла ошибка " + exception.getMessage());
@@ -33,4 +44,10 @@ public class PersonController {
         }
     }
 
+    @DeleteMapping("/delete")
+    public String delete(@RequestParam int id, Model model) {
+        personService.delete(id);
+        model.addAttribute("person", personService.findAll());
+        return "people/show";
+    }
 }
