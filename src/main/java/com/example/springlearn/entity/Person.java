10c6c34e8962ec0af8b5126bb807ee1180ad0d73
v.avdeev <v.avdeev@g.nsu.ru>
2023-11-29 19:44:58 +0300

    Written new validation annotations

--- src/main/java/com/example/springlearn/entity/Person.java
+++ src/main/java/com/example/springlearn/entity/Person.java
@@ -3,29 +3,36 @@ package com.example.springlearn.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @Setter
 @Getter
 @Table(name = "Person")
 public class Person {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
     private long id;
 
-    @Min(2)
-    private String name;
-
-    @Min(1)
+    @Min(value = 0, message = "Age should be greater than 0")
+    @Column(name = "age")
     private int age;
 
+    @Column(name = "email")
+    @NotEmpty(message = "Email should not be empty")
     @Email
     private String email;
 
-    public Person() {
-    }
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    @Column(name = "name")
+    private String name;
+
 }
