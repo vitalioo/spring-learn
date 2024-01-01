5192256b4908d2a50a93bcc118e999e9fa32307e
v.avdeev <v.avdeev@g.nsu.ru>
2024-01-01 20:17:25 +0300

    Added lombok annotation

--- src/main/java/com/example/springlearn/controllers/PersonController.java
+++ src/main/java/com/example/springlearn/controllers/PersonController.java
@@ -1,19 +1,17 @@
 package com.example.springlearn.controllers;
 
-import com.example.springlearn.entity.Person;
+import com.example.springlearn.models.Person;
 import com.example.springlearn.service.PersonService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/person")
+@RequiredArgsConstructor
 public class PersonController {
     private final PersonService personService;
-
-    public PersonController(PersonService PersonService) {
-        this.personService = PersonService;
-    }
 
     @GetMapping("/new")
     public String newPerson(Model model) {
@@ -33,7 +31,7 @@ public class PersonController {
         return "people/show";
     }
 
-    @GetMapping( "/index")
+    @GetMapping("/index")
     public String index(@RequestParam(defaultValue = "0") int id, Model model) {
         try {
             model.addAttribute("person", personService.findOne(id));
