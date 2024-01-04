d58b0adde54251ae674a2193badebc9ebc071e2a
v.avdeev <v.avdeev@g.nsu.ru>
2024-01-04 23:54:54 +0300

    Written findByName method for authentication

--- src/main/java/com/example/springlearn/repositories/PersonRepository.java
+++ src/main/java/com/example/springlearn/repositories/PersonRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Integer> {
     List<Person> findByNameOrderByAge(String name);
-
     List<Person> findByEmail(String email);
+    Optional<Person> findByName(String username);
 }
