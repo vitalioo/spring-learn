d731b4693a057fed16a742e603465ef2a7379ee7
v.avdeev <v.avdeev@g.nsu.ru>
2024-01-04 16:51:58 +0300

    Created PersonDetails

---
+++ src/main/java/com/example/springlearn/security/PersonDetails.java
@@ -0,0 +1,50 @@
+package com.example.springlearn.security;
+
+import com.example.springlearn.models.Person;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class PersonDetails implements UserDetails {
+    private Person person;
+
+    public PersonDetails(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.person.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.person.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
