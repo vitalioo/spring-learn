c167c0c23a2810f3998701531950a70b836b59f0
v.avdeev <v.avdeev@g.nsu.ru>
2024-01-05 14:27:09 +0300

    Added a ComponentScan for models due to bugs

--- src/main/java/com/example/springlearn/SpringLearnApplication.java
+++ src/main/java/com/example/springlearn/SpringLearnApplication.java
@@ -2,8 +2,10 @@ package com.example.springlearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.springlearn.models")
 public class SpringLearnApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringLearnApplication.class, args);
